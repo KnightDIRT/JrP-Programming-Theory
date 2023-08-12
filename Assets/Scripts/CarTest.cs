a7db6942cae41cf541770edf07880ba406ea92d2
KnightDIRT <knightkantinan@gmail.com>
2023-08-12 13:21:19 +0700

    Created Prototype vehicle
    -Created prototype steering type car

---
+++ Assets/Scripts/CarTest.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarTest : MonoBehaviour
+{
+    public List<AxleInfo> axleInfos;
+    public float maxMotorTorque;
+    public float maxSteeringAngle;
+    public float brakeTorque;
+
+    [SerializeField] private float measuredSpeed;
+
+    // finds the corresponding visual wheel
+    // correctly applies the transform
+    public void ApplyLocalPositionToVisuals(WheelCollider collider)
+    {
+        if (collider.transform.childCount == 0)
+        {
+            return;
+        }
+
+        Transform visualWheel = collider.transform.GetChild(0);
+
+        Vector3 position;
+        Quaternion rotation;
+        collider.GetWorldPose(out position, out rotation);
+
+        visualWheel.transform.position = position;
+        visualWheel.transform.rotation = rotation * Quaternion.Euler(0, 0, 90);
+    }
+
+    public void FixedUpdate()
+    {
+        measuredSpeed = gameObject.GetComponent<Rigidbody>().velocity.magnitude * 3.6f;
+
+        float motor = maxMotorTorque * Input.GetAxis("Vertical");
+        float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+
+        foreach (AxleInfo axleInfo in axleInfos)
+        {
+            if (axleInfo.steering)
+            {
+                axleInfo.leftWheel.steerAngle = steering * axleInfo.steeringMultiplier;
+                axleInfo.rightWheel.steerAngle = steering * axleInfo.steeringMultiplier;
+            }
+            if (axleInfo.motor)
+            {
+                axleInfo.leftWheel.motorTorque = motor;
+                axleInfo.rightWheel.motorTorque = motor;
+            }
+            if (Input.GetKey(KeyCode.Space))
+            {
+                axleInfo.leftWheel.brakeTorque = brakeTorque;
+                axleInfo.rightWheel.brakeTorque = brakeTorque;
+            }
+            else
+            {
+                axleInfo.leftWheel.brakeTorque = 0;
+                axleInfo.rightWheel.brakeTorque = 0;
+            }
+            ApplyLocalPositionToVisuals(axleInfo.leftWheel);
+            ApplyLocalPositionToVisuals(axleInfo.rightWheel);
+        }
+    }
+}
+
+[System.Serializable]
+public class AxleInfo
+{
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+    public bool motor; // is this wheel attached to motor?
+    public bool steering; // does this wheel apply steer angle?
+    public int steeringMultiplier;
+}
---
+++ Assets/Scripts/Vehicle.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public abstract class Vehicle : MonoBehaviour
+{
+    [SerializeField] private float maxSpeed = 30;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
